b7e6a07bf285d19ef9dbc7b1c5e25acfc70a1c61
georgegito <gitopoulos99@gmail.com>
2022-08-01 02:06:29 +0300

    Custom evaluation metrics

---
+++ metrics.py
@@ -0,0 +1,18 @@
+from keras import backend as K
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    _recall = true_positives / (possible_positives + K.epsilon())
+    return _recall
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    _precision = true_positives / (predicted_positives + K.epsilon())
+    return _precision
+
+def f1(y_true, y_pred):
+  _precision = precision(y_true, y_pred)
+  _recall = precision(y_true, y_pred)
+  return 2*((_precision * _recall) / (_precision + _recall + K.epsilon()))
